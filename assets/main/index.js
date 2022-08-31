44ddc40536cd8f038b8809a8a9547a2ede7efbe2
bob <sammy.chesire@student.moringaschool.com>
2022-09-01 00:31:10 +0300

    fetch data from the API

--- assets/main/index.js
+++ assets/main/index.js
@@ -1,7 +1,31 @@
-const accessKey = ('FFe9OwqmwBF2zbCZcibSgOqniyGYjNzymP_EOlEbsI4')
 
-const randomUrl = ('https://api.unsplash.com/photos/random?client_id=${accessKey}&count=30')
+ const accessKey = 'FFe9OwqmwBF2zbCZcibSgOqniyGYjNzymP_EOlEbsI4';
 
-const gallery = document.querySelector('.gallery')
+ const randomUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=30`;
 
-let allImages;  //store all images
+ const gallery = document.querySelector('.gallery')
+
+ let allImages;  //store all images
+
+ function getAllImages(){
+     (fetch(randomUrl))
+     .then((res) => res.json())
+     .then((data) => {
+        console.log(data)
+         allImages = data;
+         createImages(allImages)
+     });
+    
+ }
+
+ function createImages(data){
+     data.forEach((item,index) => {
+         let img = document.createElement('img')
+        img.src = item.urls.regular
+        img.className = 'gallery-img'
+         gallery.appendChild(img);
+     })
+ }
+
+ getAllImages();
+
